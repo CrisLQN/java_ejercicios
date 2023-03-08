001eb3c4ecc5b81edbb26f63ae64152cb13574c1
Cristian <cristiannavapro@gmail.com>
2023-03-08 13:24:13 -0300

    3

---
+++ Parte 1/Nombre_variable_mostrar/src/nombre_variable_mostrar/Nombre_variable_mostrar.java
@@ -0,0 +1,15 @@
+
+package nombre_variable_mostrar;
+import java.util.Scanner;
+
+public class Nombre_variable_mostrar {
+
+   
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Ingresa tu nombre");
+        String nombre=leer.nextLine();
+        System.out.println("Tu nombre es "+nombre);
+    }
+    
+}
